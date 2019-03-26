e6509928430f0609da2876696ea95da393d25906
hrochoa <hrochoa@live.com>
2019-03-26 00:05:21 -0500

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PhoneApp10.Resources;
+using Microsoft.Devices;
+using System.Windows.Media.Imaging;
+using System.Threading;
+namespace PhoneApp10
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        PhotoCamera cam = new PhotoCamera();
+      
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+
+            
+            // Check to see if the camera is available on the phone.
+            if ((PhotoCamera.IsCameraTypeSupported(CameraType.Primary) == true) ||
+                 (PhotoCamera.IsCameraTypeSupported(CameraType.FrontFacing) == true))
+            {
+                // Initialize the default camera.
+                cam = new Microsoft.Devices.PhotoCamera();
+
+                //Event is fired when the PhotoCamera object has been initialized
+                cam.Initialized += new EventHandler<Microsoft.Devices.CameraOperationCompletedEventArgs>(cam_Initialized);
+
+                //Set the VideoBrush source to the camera
+                viewfinderBrush.SetSource(cam);
+            }
+            else
+            {
+                // The camera is not supported on the phone.
+                this.Dispatcher.BeginInvoke(delegate()
+                {
+                    // Write message.
+                    txtDebug.Text = "A Camera is not available on this phone.";
+                });
+            }
+        }
+        protected override void OnNavigatingFrom(System.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+            if (cam != null)
+            {
+                // Dispose of the camera to minimize power consumption and to expedite shutdown.
+                cam.Dispose();
+
+                // Release memory, ensure garbage collection.
+                cam.Initialized -= cam_Initialized;
+            }
+        }
+        void cam_Initialized(object sender, Microsoft.Devices.CameraOperationCompletedEventArgs e)
+        {
+            if (e.Succeeded)
+            {
+                this.Dispatcher.BeginInvoke(delegate()
+                {
+                    txtDebug.Text = "Camera initialized";
+                });
+
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void f_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
