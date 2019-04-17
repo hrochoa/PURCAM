354984a690fd7d7c5e5f214f8ca0cf3d4c2f9144
hrochoa <hrochoa@live.com>
2019-04-16 20:08:14 -0500

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -11,10 +11,17 @@ using PhoneApp10.Resources;
 using Microsoft.Devices;
 using System.Windows.Media.Imaging;
 using System.Threading;
+using Microsoft.Devices;
+using System.IO;
+using System.IO.IsolatedStorage;
+using Microsoft.Xna.Framework.Media;
+using System.Windows.Media;
 namespace PhoneApp10
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        private int savedCounter = 0;
+        MediaLibrary library = new MediaLibrary();
         PhotoCamera cam = new PhotoCamera();
       
         // Constructor
@@ -71,7 +78,34 @@ namespace PhoneApp10
 
             }
         }
+        // Ensure that the viewfinder is upright in LandscapeRight.
+        protected override void OnOrientationChanged(OrientationChangedEventArgs e)
+        {
+            if (cam != null)
+            {
+                // LandscapeRight rotation when camera is on back of phone.
+                int landscapeRightRotation = 180;
 
+                // Change LandscapeRight rotation for front-facing camera.
+                if (cam.CameraType == CameraType.FrontFacing) landscapeRightRotation = -180;
+
+                // Rotate video brush from camera.
+                if (e.Orientation == PageOrientation.LandscapeRight)
+                {
+                    // Rotate for LandscapeRight orientation.
+                    viewfinderBrush.RelativeTransform =
+                        new CompositeTransform() { CenterX = 0.5, CenterY = 0.5, Rotation = landscapeRightRotation };
+                }
+                else
+                {
+                    // Rotate for standard landscape orientation.
+                    viewfinderBrush.RelativeTransform =
+                        new CompositeTransform() { CenterX = 0.5, CenterY = 0.5, Rotation = 0 };
+                }
+            }
+
+            base.OnOrientationChanged(e);
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
@@ -82,5 +116,130 @@ namespace PhoneApp10
 
         }
 
+        private void settings_Click(object sender, RoutedEventArgs e)
+        {
+            if (cam != null)
+            {
+                try
+                {
+                    // Start image capture.
+                    cam.CaptureImage();
+                }
+                catch (Exception ex)
+                {
+                    this.Dispatcher.BeginInvoke(delegate()
+                    {
+                        // Cannot capture an image until the previous capture has completed.
+                        txtDebug.Text = ex.Message;
+                    });
+                }
+            }
+
+        }
+        void cam_CaptureCompleted(object sender, CameraOperationCompletedEventArgs e)
+        {
+            // Increments the savedCounter variable used for generating JPEG file names.
+            savedCounter++;
+        }
+        void cam_CaptureImageAvailable(object sender, Microsoft.Devices.ContentReadyEventArgs e)
+        {
+            string fileName = savedCounter + ".jpg";
+
+            try
+            {   // Write message to the UI thread.
+                Deployment.Current.Dispatcher.BeginInvoke(delegate()
+                {
+                    txtDebug.Text = "Captured image available, saving photo.";
+                });
+
+                // Save photo to the media library camera roll.
+                library.SavePictureToCameraRoll(fileName, e.ImageStream);
+
+                // Write message to the UI thread.
+                Deployment.Current.Dispatcher.BeginInvoke(delegate()
+                {
+                    txtDebug.Text = "Photo has been saved to camera roll.";
+
+                });
+
+                // Set the position of the stream back to start
+                e.ImageStream.Seek(0, SeekOrigin.Begin);
+
+                // Save photo as JPEG to the local folder.
+                using (IsolatedStorageFile isStore = IsolatedStorageFile.GetUserStoreForApplication())
+                {
+                    using (IsolatedStorageFileStream targetStream = isStore.OpenFile(fileName, FileMode.Create, FileAccess.Write))
+                    {
+                        // Initialize the buffer for 4KB disk pages.
+                        byte[] readBuffer = new byte[4096];
+                        int bytesRead = -1;
+
+                        // Copy the image to the local folder. 
+                        while ((bytesRead = e.ImageStream.Read(readBuffer, 0, readBuffer.Length)) > 0)
+                        {
+                            targetStream.Write(readBuffer, 0, bytesRead);
+                        }
+                    }
+                }
+
+                // Write message to the UI thread.
+                Deployment.Current.Dispatcher.BeginInvoke(delegate()
+                {
+                    txtDebug.Text = "Photo has been saved to the local folder.";
+
+                });
+            }
+            finally
+            {
+                // Close image stream
+                e.ImageStream.Close();
+            }
+
+        }
+
+        // Informs when thumbnail photo has been taken, saves to the local folder
+        // User will select this image in the Photos Hub to bring up the full-resolution. 
+        public void cam_CaptureThumbnailAvailable(object sender, ContentReadyEventArgs e)
+        {
+            string fileName = savedCounter + "_th.jpg";
+
+            try
+            {
+                // Write message to UI thread.
+                Deployment.Current.Dispatcher.BeginInvoke(delegate()
+                {
+                    txtDebug.Text = "Captured image available, saving thumbnail.";
+                });
+
+                // Save thumbnail as JPEG to the local folder.
+                using (IsolatedStorageFile isStore = IsolatedStorageFile.GetUserStoreForApplication())
+                {
+                    using (IsolatedStorageFileStream targetStream = isStore.OpenFile(fileName, FileMode.Create, FileAccess.Write))
+                    {
+                        // Initialize the buffer for 4KB disk pages.
+                        byte[] readBuffer = new byte[4096];
+                        int bytesRead = -1;
+
+                        // Copy the thumbnail to the local folder. 
+                        while ((bytesRead = e.ImageStream.Read(readBuffer, 0, readBuffer.Length)) > 0)
+                        {
+                            targetStream.Write(readBuffer, 0, bytesRead);
+                        }
+                    }
+                }
+
+                // Write message to UI thread.
+                Deployment.Current.Dispatcher.BeginInvoke(delegate()
+                {
+                    txtDebug.Text = "Thumbnail has been saved to the local folder.";
+
+                });
+            }
+            finally
+            {
+                // Close image stream
+                e.ImageStream.Close();
+            }
+        }
     }
 }
